92aa6af8ac99eba1b281e8280b8dbe271fc54644
Mexanik-v-shortax <arimo_kisho@vk.com>
2023-05-23 13:58:27 +0300

    Zagruzil

---
+++ Zadanie2Practic2/Zadanie2Practic2/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie2Practic2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void BtnOK_Click(object sender, EventArgs e) 
+        { string str = TbNumberA1.Text; char[] a = str.ToCharArray(); TextBlockAnswer.Text = $"{a[1]}{a[2]}{a[0]}"; }
+    }
+}
